9f8849d01ed1c73de51c31e9ec7cf21355e9a6e6
Muhammad imran <120652512+MuhammadImran2623@users.noreply.github.com>
2025-06-02 20:45:35 +0530

    Add files via upload

---
+++ Day1/Helloworld.py
@@ -0,0 +1,23 @@
+# What is Python?
+# Python is High level, interactive, interpreted, object oriented scripting langugae 
+# Python is scripting language with programming capabilities 
+# python supports 3 types of programming approaches they are SCRIPTING, FUNCTION ORIENTATION PROGRAMMING 
+#  & OBJECT ORIENTATION PROGRAMMING
+# FATHER OF PYTHON IS GUIDO VAN RUSSOM ----- he invented python by the series  named 
+# Monty Python's Flying Circus in the late 1980's and early 1990's 
+# official python release  launch date was FEB 20 1991 and the first version is 1.0
+
+# Python is used to develop 
+# Standalone applications  
+# dynamic web applications 
+# Gaming applications 
+# mostly used in Automation testing  tools 
+# Now Python is vastly used in Data Science,
+# Data Analytics, 
+# Machine Learning,
+# Internet of things, Artificail Intelligence.
+
+
+"""Task of the day """
+#Print Helloworld usning your name
+print(f"Hello world welcome to  the 30 days of Python Challenge - {input()}")
